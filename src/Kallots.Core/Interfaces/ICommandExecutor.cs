50a48a780400c6f64a33acc138b853157ee51389
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-06-05 08:33:55 -0300

    refactor(ICommand): Change ExecuteCommand return type to bool

--- src/Kallots.Core/Interfaces/ICommandExecutor.cs
+++ src/Kallots.Core/Interfaces/ICommandExecutor.cs
@@ -4,6 +4,8 @@ namespace Kallots.Core.Interfaces
     {
         // Executes OS level commands (e.g., opening applications) based on the LLM intent
         // This is synchronous (void) as firing a process usually doesn't need to block the thread
-        void ExecuteCommand(string commandIntent);
+bool ExecuteCommand(string commandIntent);
+
+        
     }
 }
